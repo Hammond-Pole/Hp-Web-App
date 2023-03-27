d73e6142793a32c8084f57eb87be6ebfdfe88630
Alan Jordaan <AlanJ@hammondpole.co.za>
2023-03-27 11:54:34 +0200

    Settings formatted and tested. Just upload left to do.

--- Hp-Web-App.Shared/Models/Document.cs
+++ Hp-Web-App.Shared/Models/Document.cs
@@ -1,9 +1,15 @@
-﻿namespace Hp_Web_App.Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hp_Web_App.Shared.Models
 {
     public class Document
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(25, ErrorMessage = "Name is too long.")]
         public string? Name { get; set; }
+        [Required]
+        [StringLength(250, ErrorMessage = "Desc is too long.")]
         public string? Description { get; set; }
         public ICollection<QuestionField>? QuestionFields { get; set; } // navigation property
         public ICollection<CompanyDocument>? CompanyDocuments { get; set; } // navigation property
--- Hp-Web-App.Shared/Models/User.cs
+++ Hp-Web-App.Shared/Models/User.cs
@@ -1,13 +1,25 @@
-﻿namespace Hp_Web_App.Shared.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hp_Web_App.Shared.Models;
 
 public class User
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(25, ErrorMessage = "Name is too long.")]
     public string? Name { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Email is too long.")]
+    // Add a validation with a message that if the Email is not a valid email it should show "Not a valid email".
+    [EmailAddress(ErrorMessage = "Not a valid email")]
     public string? Email { get; set; }
+    [Required]
     public string? Password { get; set; }
+    [Required]
     public int UserRoleId { get; set; }
+    [Required]
     public int CompanyId { get; set; }
+    [Required]
     public bool IsActive { get; set; }
     public Company? Company { get; set; } // navigation property
     public UserRole? UserRole { get; set; }
