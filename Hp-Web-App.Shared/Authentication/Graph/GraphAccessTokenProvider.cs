909f7d7fff70484006b237cb1de2138827aa919c
Alan Jordaan <AlanJ@hammondpole.co.za>
2023-05-10 12:40:30 +0200

    Added Graph Funtionality for Emails

---
+++ Hp-Web-App.Shared/Authentication/Graph/GraphAccessTokenProvider.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using Microsoft.Kiota.Abstractions.Authentication;
+
+namespace Hp_Web_App.Shared.Authentication.Graph;
+
+public class GraphAccessTokenProvider : IAccessTokenProvider
+{
+    private readonly TokenCredential _credential;
+    private readonly AllowedHostsValidator _allowedHostsValidator;
+
+    public GraphAccessTokenProvider(TokenCredential credential)
+    {
+        _credential = credential;
+        _allowedHostsValidator = new AllowedHostsValidator(new[] { "graph.microsoft.com" });
+    }
+
+    public AllowedHostsValidator AllowedHostsValidator => _allowedHostsValidator;
+
+    public async Task<string> GetAuthorizationTokenAsync(Uri uri, Dictionary<string, object>? additionalAuthenticationContext = null, CancellationToken cancellationToken = default)
+    {
+        if (!_allowedHostsValidator.IsUrlHostValid(uri))
+        {
+            return string.Empty;
+        }
+
+        var tokenRequestContext = new TokenRequestContext(new[] { "https://graph.microsoft.com/.default" });
+        var token = await _credential.GetTokenAsync(tokenRequestContext, cancellationToken);
+        return token.Token;
+    }
+}
---
+++ Hp-Web-App.Shared/ClientFactories/GraphClientFactory.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Identity;
+using Hp_Web_App.Shared.Authentication.Graph;
+using Microsoft.Graph;
+using Microsoft.Kiota.Abstractions.Authentication;
+
+namespace Hp_Web_App.Shared.ClientFactories;
+
+public class GraphClientFactory : IGraphClientFactory
+{
+    public GraphClientFactory()
+    {
+    }
+
+    public GraphServiceClient CreateClient(string clientId, string clientSecret, string tenantId)
+    {
+        // Setup the connection to Azure.
+        var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+        // Get a token.
+        var token = new GraphAccessTokenProvider(credential);
+
+        // Set the authentication provider.
+        var authProvider = new BaseBearerTokenAuthenticationProvider(token);
+
+        GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+        return graphClient;
+    }
+}
