303050d5b9dcc592242fdee701306c19ea763b76
Alan Jordaan <AlanJ@hammondpole.co.za>
2023-05-22 14:28:22 +0200

    #32 Added Name and Description properties to UserRole class, along with a navigation property for users. These will show the name in the dynamic table.

--- Hp-Web-App.Shared/Models/UserRole.cs
+++ Hp-Web-App.Shared/Models/UserRole.cs
@@ -1,9 +1,25 @@
-﻿namespace Hp_Web_App.Shared.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hp_Web_App.Shared.Models;
 
 public class UserRole
 {
+    #region Primary Key
+    [Required]
+    [ExcludeFromTable]
     public int Id { get; set; }
+    #endregion
+
+    #region User Properties
+    [StringLength(25, ErrorMessage = "Name is too long.")]
     public string Name { get; set; } = string.Empty;
+
+    [StringLength(255, ErrorMessage = "Description is too long.")]
     public string Description { get; set; } = string.Empty;
+    #endregion
+
+    #region Navigation Properties
+    [ExcludeFromTable]
     public ICollection<User>? Users { get; set; } // navigation property
+    #endregion
 }
